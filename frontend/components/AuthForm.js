b2bc72c944870ed1c0e5fc1817163d719e5a624e
Timothy Renteria <Timothyxrenteria@gmail.com>
2024-05-13 22:18:11 -0700

    finished

--- frontend/components/AuthForm.js
+++ frontend/components/AuthForm.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 export default function AuthForm() {
+  const navigate = useNavigate()
   const [isLogin, setIsLogin] = useState(true)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -19,6 +22,27 @@ export default function AuthForm() {
     setPassword(event.target.value)
   }
 
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    setError('')
+    setMessage('')
+    try {
+      const { data } = await axios.post(
+        `/api/auth/${isLogin ? 'login' : 'register'}`, 
+        { username, password }
+      )
+      if (isLogin) {
+        localStorage.setItem('token', data.token)
+        navigate('/stars')
+      } else {
+        setMessage(data.message)
+      }
+    }
+    catch (err) {
+      setError(err?.response?.data?.message) || 
+      'An error occurred. Please try again!'
+    }
+  }
   return (
     <div className="container">
       <div aria-live="polite">{message}</div>
@@ -28,7 +52,7 @@ export default function AuthForm() {
           Switch to {isLogin ? 'Register' : 'Login'}
         </button>
       </h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username:</label>
           <input
